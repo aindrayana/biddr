b45883d8ba5fb5d32b01269574fc4f3fe8eb9e16
Aji Indrayana <ajivancouver@gmail.com>
2015-12-11 11:38:42 -0800

    rspec test drive for auction model and controller

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,9 +1,33 @@
 class AuctionsController < ApplicationController
+  before_action :authenticate_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_auction, only: [:edit, :update, :destroy, :show]
 
   def index
   end
 
   def new
+    @auction = Auction.new
+  end
+
+  def create
+    @auction = Auction.new auction_params
+    @auction.user = current_user
+    if @auction.save
+      redirect_to auction_path(@auction)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def find_auction
+    @auction = Auction.find params[:id]
+  end
+
+  def auction_params
+    params.require(:auction).permit(:title, :detail, :end_date, :end_date, :price)
   end
 
 end
--- spec/models/auction_spec.rb
+++ spec/models/auction_spec.rb
@@ -3,14 +3,16 @@ require 'rails_helper'
 RSpec.describe Auction, type: :model do
 
   describe "validations" do
+    # rspec spec/models/auction_spec.rb:7
     it "require a title" do
-      auction = Action.new
+      auction = Auction.new
       auction.valid?
       expect(auction.errors.messages).to have_key(:title)
     end
 
+    # rspec spec/models/auction_spec.rb:14
     it "require a reserve price" do
-      auction = Action.new
+      auction = Auction.new
       auction.valid?
       expect(auction.errors.messages).to have_key(:price)
     end
